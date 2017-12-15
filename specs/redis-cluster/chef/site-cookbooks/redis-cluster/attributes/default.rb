7eba5db821969ee2db1e0d5425293ba14eb37c0d
Bryan Berry <v-bryber@microsoft.com>
2017-12-15 11:37:12 +0000

    remove include_attribute as the updated sysctl cookbook no longer has attributes files

--- specs/redis-cluster/chef/site-cookbooks/redis-cluster/attributes/default.rb
+++ specs/redis-cluster/chef/site-cookbooks/redis-cluster/attributes/default.rb
@@ -19,7 +19,5 @@ default['redis']['replicas'] = nil
 default['redis']['mb_per_slot'] = 4000
 default['redis']['cluster-node-timeout'] = 5000
 
-include_attribute 'sysctl'
-
 default['redis']['sysctl']['vm']['overcommit_memory'] = 1
 default['redis']['sysctl']['net']['core']['somaxconn'] = 512
